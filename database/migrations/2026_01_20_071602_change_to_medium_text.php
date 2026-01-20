f66a6feffb886c864834c76603c0ea588dd737fc
Marcus Andersson <marcus.andersson@rolf.se>
2026-01-20 08:22:30 +0100

    klassnamnskrock

--- database/migrations/2026_01_20_071602_change_to_medium_text.php
+++ database/migrations/2026_01_20_071602_change_to_medium_text.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class ChangeHtmlContentToMediumText20260120 extends Migration
+return new class extends Migration
 {
     /**
      * Run the migrations.
@@ -41,4 +41,4 @@ class ChangeHtmlContentToMediumText20260120 extends Migration
             $table->text('html_content')->change();
         });
     }
-}
+};
