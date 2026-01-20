593655f477b609cb82aea4f70b0f7ab81800ed25
Marcus Andersson <marcus.andersson@rolf.se>
2026-01-20 08:16:52 +0100

    html_content text -> medium text

---
+++ database/migrations/2026_01_20_071602_change_to_medium_text.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class UpdateHtmlContentToMediumtext extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up()
+    {
+        Schema::table('exercises', function (Blueprint $table) {
+            $table->mediumText('html_content')->change();
+        });
+
+        Schema::table('recepies', function (Blueprint $table) {
+            $table->mediumText('html_content')->change();
+        });
+
+        Schema::table('training_schedules', function (Blueprint $table) {
+            $table->mediumText('html_content')->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down()
+    {
+        Schema::table('exercises', function (Blueprint $table) {
+            $table->text('html_content')->change();
+        });
+
+        Schema::table('recepies', function (Blueprint $table) {
+            $table->text('html_content')->change();
+        });
+
+        Schema::table('training_schedules', function (Blueprint $table) {
+            $table->text('html_content')->change();
+        });
+    }
+}
